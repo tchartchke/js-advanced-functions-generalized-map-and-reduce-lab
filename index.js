d2f0e04e024f94e92ea8ae358fe035faaf80f29a
tchartchke <chartai.kl@gmail.com>
2021-04-19 15:12:30 -0500

    commit tree 8a210cb9ca4151c499c02eba4080700be96aae6d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(arr, fN){
+  let r = []
+  for (let i = 0; i < arr.length; i++ ) {
+    r.push(fN(arr[i]))
+  }
+  return r
+}
+
+function reduce(arr, fN, start = 0){
+  let ret = start ? start : arr[0]
+
+  for (let i = start ? 0 : 1 ; i < arr.length; i++ ) {
+    ret = fN(arr[i], ret)
+  }
+  return ret;
+}
